b27eea131f11367a0dda77d38f2b0a83e1e61bd5
Okoko Anainga <osa37@uclive.ac.nz>
2021-10-08 12:57:21 +1300

    Basic Setup for functions

--- final/game.c
+++ final/game.c
@@ -1,5 +1,32 @@
 #include "system.h"
 
+
+void characterObject(void) 
+{
+
+}
+
+void highObject(void) 
+{
+
+}
+
+void lowObject(void) 
+{
+
+}
+
+void jump(void)
+{
+
+}
+
+void duck(void)
+{
+    
+}
+
+
 int main (void)
 {
     system_init ();
