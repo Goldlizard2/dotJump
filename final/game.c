2ec4f6abd3090904a2883bf745d20a9716ec9562
Okoko Anainga <osa37@uclive.ac.nz>
2021-10-08 13:21:42 +1300

    Update final/game.c

--- final/game.c
+++ final/game.c
@@ -1,27 +1,38 @@
 #include "system.h"
 
 
-void characterObject(void) 
+void tallCharacterObject(void)
+/*three led high object for a character model*/ 
 {
 
 }
 
-void highObject(void) 
+void lowCharacherObject(void)
+/*two led high state that is entered when in the ducking state from pulling LED down*/
 {
 
 }
 
-void lowObject(void) 
+void highObject(void)
+/*3 led object from the roof that can be ducked under*/
+{
+
+}
+
+void lowObject(void)
+/*one led object along the ground that can be jumped*/ 
 {
 
 }
 
 void jump(void)
+/*jump moves the character object up two leds then falls*/
 {
 
 }
 
 void duck(void)
+/*duck moves the character object to lowCharacherObject and then resets to tallCharacterObject*/
 {
     
 }
