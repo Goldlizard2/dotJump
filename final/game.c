ee9aa0e001b351ee779300f612fe491cb49bb83a
Daniel Pallesen <dpa107@uclive.ac.nz>
2021-10-08 14:55:52 +1300

    Pressing navswitch in on button 1 will change screen

--- final/game.c
+++ final/game.c
@@ -2,10 +2,22 @@
 #include "tinygl.h"
 #include "pacer.h"
 #include "../fonts/font5x7_1.h"
+#include "navswitch.h"
+#include "button.h"
+#include "pio.h"
 
 #define MESSAGE_RATE 10
 #define PACER_RATE 500
 #define MENU_TEXT "WELCOME"
+
+
+#define BUTTON_PIO PIO_DEFINE(PORT_D, 7)
+int button_pressed_p (void)
+{
+    return pio_input_get(BUTTON_PIO);
+}
+
+
 
 void initialize(void)
 {
@@ -14,6 +26,8 @@ void initialize(void)
     tinygl_font_set(&font5x7_1);
     tinygl_text_speed_set(MESSAGE_RATE);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
+    // Init button 1
+    pio_config_set(BUTTON_PIO, PIO_INPUT);
 }
 
 void displayMenu(void)
@@ -60,16 +74,24 @@ void duck(void)
 
 int main (void)
 {
+    // Initalize game and display menu
     system_init();
     initialize();
     displayMenu();
 
-    while (1)
+    // Update menu
+    while (!navswitch_push_event_p(NAVSWITCH_PUSH) && !button_pressed_p())
     {
         pacer_wait();
         tinygl_update();
+        navswitch_update();
+        button_update();
+    }
 
-
-
+    // GAME LOGIC HERE
+    tinygl_text("GAME");
+    while (1) {
+        pacer_wait();
+        tinygl_update();
     }
 }
